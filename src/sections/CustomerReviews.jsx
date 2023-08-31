a9a279c5328764822a3f80da36bdfb303cca3dd3
dopelyner <dalyner@protonmail.com>
2023-08-31 12:19:53 +0000

    first commit

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,15 @@
+
+const CustomerReviews = () => {
+  return (
+    <section className="max-container">
+       <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say ?
+       </h3>
+       <p className="info-text m-auto mt-4 max-x-lg text-center">Hear genuine stories from our satisf customers about theirs exceptional experiences with us.</p>
+    </section>
+  )
+}
+
+export default CustomerReviews
